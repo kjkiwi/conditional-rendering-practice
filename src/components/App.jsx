2abea34659942063985e3f3e6d42f1139e3a40e4
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-12 15:24:03 +1200

    finished showinb both versions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Form from "./Form";
+//import Input from "./Input";
 
 var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      <Form />
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
+
+//My very long way of doing it
+// var userIsRegistered = false;
+
+// function App() {
+//   return (
+//     <div className="container">
+//       {userIsRegistered ? (
+//         <form className="form">
+//           <Input type="text" placeholder="Username" />
+//           <Input type="password" placeholder="Password" />
+//           <button id="button" type="submit">
+//             Login
+//           </button>
+//         </form>
+//       ) : (
+//         <Form />
+//       )}
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+//not necessary
+
+// import React from "react";
+
+// function Input(props) {
+//   return <input type={props.type} placeholder={props.placeholder} />;
+// }
+
+// export default Input;
